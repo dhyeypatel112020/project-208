3290418616745b784a3c9a003e6f1bb1089d80e6
dhyeypatel112020 <87629011+dhyeypatel112020@users.noreply.github.com>
2022-09-12 08:35:49 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+from threading import Thread    
+
+IP_ADDRESS ='127.0.0.1'
+PORT = 8050
+SERVER = None
+BUFFER_SIZE = 4096
+clients = {}
+
+
+
+def setup ( ) :
+    print ( " \n\t\t\t\t\t\t IP MESSENGER \ n " )
+    
+    # Getting global values
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+    SERVER = socket.socket ( socket.AF_INET , socket . SOCK_STREAM )
+    SERVER.bind ( ( IP_ADDRESS , PORT ) )
+    
+    # Listening incomming connections
+    SERVER.listen ( 100 )
+    
+    print ( " \t\t\t\t SERVER IS WAITING FOR INCOMMING CONNECTIONS ... " )
+    print ( " \n " )
+
+    acceptConnections()
+
+setup_thread = Thread(target=setup)
+setup_thread.start()
+
+def acceptConnections():
+    global SERVER
+    global clients
+    
+    while True :
+        client , addr = SERVER.accept()
+        print (client.addr)
